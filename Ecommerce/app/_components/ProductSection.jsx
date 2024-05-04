6730158272d6f2fe7cf9bf2ae79c9f0f77cd64f0
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-05 01:54:07 +0300

    Fetch Products and assign backed data to the Product List component

--- Ecommerce/app/_components/ProductSection.jsx
+++ Ecommerce/app/_components/ProductSection.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { ProductList } from './ProductList'
+import ProductApis from '../_utils/ProductApis'
 
 export const ProductSection = () => {
+
+  const [productList, setProductList] = useState([])
+
+  const getLatestProducts_ = () => {
+    ProductApis.getLatestProducts().then(res => {
+      setProductList(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getLatestProducts_()
+  }, [])
+
+
+
   return (
-    <div>ProductSection</div>
+    <div>
+      {
+        productList.length > 0 &&
+        <ProductList List={productList} />}
+    </div>
   )
 }
