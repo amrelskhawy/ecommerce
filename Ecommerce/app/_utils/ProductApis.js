8a24742ce99e80d2d9ac108bbb6010f0db1a37db
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-05 18:07:08 +0300

    Added method to fetch product by ID

--- Ecommerce/app/_utils/ProductApis.js
+++ Ecommerce/app/_utils/ProductApis.js
@@ -3,7 +3,10 @@ import  axiosClient from "./axiosClient";
 
 const getLatestProducts = () => axiosClient.get('/products?populate=*').then(res => res.data)
 
+const getProductByID = (id) => axiosClient.get(`/products/${id}?populate=*`).then(res => res.data)
+
 
 export default {
-    getLatestProducts
+    getLatestProducts,
+    getProductByID
 }
