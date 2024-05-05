98aacc7834efa13c0e43c089067d1c0493aee6ac
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-06 00:43:29 +0300

    start using components and real data back from API

--- Ecommerce/app/product-details/[productID]/page.jsx
+++ Ecommerce/app/product-details/[productID]/page.jsx
@@ -1,6 +1,10 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import ProductApis from '../../_utils/ProductApis'
+import Image from "next/image";
+import { Breadcrumb } from "@/app/_components/Breadcrumb";
+import { ProductInfo } from "./_components/ProductInfo";
+import { ProductBanner } from "./_components/ProductBanner";
 
 export default function ProductDetail({ params }) {
 
@@ -20,12 +24,16 @@ export default function ProductDetail({ params }) {
 
 
 	if (product && product.attributes) {
-		const { title, description, category, price, banner } = product?.attributes
-		const { url } = banner?.data?.attributes?.formats?.thumbnail
+		const { title, banner } = product?.attributes
+		const { url } = banner?.data.attributes
 		return (
-			<div>
-				<h2>{title}</h2>
-				<p>{description}</p>
+			<div className="container  md:p-0 p-3 grid gap-4 mx-auto mt-12">
+				{/* Breadcrumb */}
+				<Breadcrumb productTitle={title} />
+				<div className="grid md:grid-cols-2 gap-6">
+					<ProductBanner banner={url} />
+					<ProductInfo product={product} />
+				</div>
 			</div>
 		)
 	}
