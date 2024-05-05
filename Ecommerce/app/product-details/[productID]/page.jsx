d43a8f2f29346adc791a95ee6360cb68cfeb184e
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-05 19:24:40 +0300

    fetch data based on the product ID and assign it to product

--- Ecommerce/app/product-details/[productID]/page.jsx
+++ Ecommerce/app/product-details/[productID]/page.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
+import ProductApis from '../../_utils/ProductApis'
 
 export default function ProductDetail({ params }) {
-	return (
-		<div>
-			{params?.productID}
-		</div>
-	)
+
+	const [product, setProduct] = useState({})
+
+	const { productID } = params
+
+	const getProductByID_ = () => {
+		ProductApis.getProductByID(productID).then(res => {
+			setProduct(res.data);
+		})
+	}
+
+	useEffect(() => {
+		getProductByID_()
+	}, [productID])
+
+
+	if (product && product.attributes) {
+		const { title, description, category, price, banner } = product?.attributes
+		const { url } = banner?.data?.attributes?.formats?.thumbnail
+		return (
+			<div>
+				<h2>{title}</h2>
+				<p>{description}</p>
+			</div>
+		)
+	}
+
+
 }
