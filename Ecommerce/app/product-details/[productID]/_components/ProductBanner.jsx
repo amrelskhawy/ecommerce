f5e69dc63b69465b3eb70cdb46c2fda6a4110ba8
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-06 22:37:06 +0300

    reformating the components

--- Ecommerce/app/product-details/[productID]/_components/ProductBanner.jsx
+++ Ecommerce/app/product-details/[productID]/_components/ProductBanner.jsx
@@ -2,15 +2,15 @@ import Image from 'next/image'
 import React from 'react'
 
 export const ProductBanner = ({ banner }) => {
-    return (
-        <div className='rounded-xl group/item overflow-hidden'>
-            <Image
-                src={banner}
-                alt="banner"
-                width={500}
-                height={500}
-                className="w-full transition-all h-full group-hover/item:scale-105"
-            />
-        </div>
-    )
+  return (
+    <div className='rounded-xl group/item overflow-hidden'>
+      <Image
+        src={banner}
+        alt="banner"
+        width={500}
+        height={500}
+        className="w-full transition-all h-full group-hover/item:scale-105"
+      />
+    </div>
+  )
 }
