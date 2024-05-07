c2e86ee9db57249d795e02ea52536eed9715a30e
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-08 00:57:28 +0300

    initilaze clerk authenticating with adding routes and middlewares

--- Ecommerce/app/layout.js
+++ Ecommerce/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+import { ClerkProvider } from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,12 +13,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-          {children}
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+            {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
