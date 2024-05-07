dbb764fcc5e8c998a6a714cc437d1df13c95aa31
Amr Elskhawy <amrelskhawy@gmail.com>
2024-05-08 00:22:17 +0300

    added skeleton animation

--- Ecommerce/app/product-details/[productID]/_components/ProductBanner.jsx
+++ Ecommerce/app/product-details/[productID]/_components/ProductBanner.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 
 export const ProductBanner = ({ banner }) => {
   return (
-    <div className='rounded-xl group/item overflow-hidden'>
-      <Image
+    <div className='rounded-xl group/item overflow-hidden relative'>
+      {
+        banner ?
+        <Image
         src={banner}
         alt="banner"
         width={500}
         height={500}
         className="w-full transition-all h-full group-hover/item:scale-105"
-      />
+      /> :
+      <div className='w-full h-full animate-pulse bg-slate-200 '></div>}
     </div>
   )
 }
--- Ecommerce/app/product-details/[productID]/_components/ProductInfo.jsx
+++ Ecommerce/app/product-details/[productID]/_components/ProductInfo.jsx
@@ -4,32 +4,53 @@ import React from 'react'
 export const ProductInfo = ({ product }) => {
   const { title, instantDelivery, description, category, price } = product?.attributes
   return (
-    <div className="flex flex-col gap-2">
-      <h2 className="text-2xl font-medium">{title}</h2>
-      <p className="font-bold text-slate-400 w-fit  ">
-        {category}</p>
-      <p className='text-lg font-medium  text-slate-800'>{description}</p>
-      
-      <p className='flex items-center text-slate-600 font-medium gap-2'>
-        {
-          instantDelivery ?
-          <BadgeCheck className='text-green-500' size={18} />
-          :
-          <AlertOctagon className='text-red-500' size={18} />
-        }
-        {
-          !instantDelivery &&
-          'Not '
-        }
-        Eligible for instant delivery
-      </p>
-      <p className='text-2xl'>Amout:
-        <span className='text-primary font-bold '> ${price}</span>
-      </p>
-      <button className='bg-primary hover:bg-primary/90 transition-all flex justify-center items-center gap-2 text-white p-3 rounded-lg mt-4'>
-        <ShoppingCart />
-        Add to cart
-      </button>
-    </div>
+    <>
+      {
+        product ?
+        <div className="flex flex-col gap-2">
+        <h2 className="text-2xl font-medium">{title}</h2>
+        <p className="font-bold text-slate-400 w-fit  ">
+          {category}</p>
+        <p className='text-lg font-medium  text-slate-800'>{description}</p>
+
+        <p className='flex items-center text-slate-600 font-medium gap-2'>
+          {
+            instantDelivery ?
+              <BadgeCheck className='text-green-500' size={18} />
+              :
+              <AlertOctagon className='text-red-500' size={18} />
+          }
+          {
+            !instantDelivery &&
+            'Not '
+          }
+          Eligible for instant delivery
+        </p>
+        <p className='text-2xl'>Amout:
+          <span className='text-primary font-bold '> ${price}</span>
+        </p>
+        <button className='bg-primary hover:bg-primary/90 transition-all flex justify-center items-center gap-2 text-white p-3 rounded-lg mt-4'>
+          <ShoppingCart />
+          Add to cart
+        </button>
+      </div>
+      :
+      <ProductInfoSkeleton />}
+    </>
+  )
+}
+
+
+const ProductInfoSkeleton = () => {
+  return (
+    <div className="grid gap-1 items-center">
+    <div className="h-[18px] animate-pulse w-[250px] bg-slate-200"/>
+    <div className="h-[18px] animate-pulse w-[50px] bg-slate-200"/>
+    <div className='h-[18px] animate-pulse w-[200px] bg-slate-200'/>
+
+    <div className='h-[18px] animate-pulse w-[200px] bg-slate-200' />
+    <div className='h-[18px] animate-pulse w-[200px] bg-slate-200' />
+    <div className='h-[18px] animate-pulse w-[100px] bg-slate-200' />
+  </div>
   )
 }
